595dd551acb7972d436cc6fd0e44d4666ad49e66
venoprotective <venoprotective@gmail.com>
2026-04-08 17:20:03 +0300

    plot decision regions func + miniTest new func

--- fastTest.py
+++ fastTest.py
@@ -2,13 +2,24 @@ import matplotlib.pyplot as plt
 
 from perceptron import Perceptron
 from IrisDataSet import *
+from plot_decision_regions import plot_decision_regions
+
 
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
 
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel("Epochs")
-plt.ylabel("Amount errors")
-plt.grid(True)
-plt.tight_layout()
+# ошибки от эпох
+# plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+# plt.xlabel("Epochs")
+# plt.ylabel("Amount errors")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+# test plot decision regions func
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel("длина чашел.")
+plt.ylabel("длина лепестка")
+plt.legend(loc='upper left')
 plt.show()
