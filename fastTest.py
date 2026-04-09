80a40fae9d26163847aa0ceac81676172961b032
venoprotective <venoprotective@gmail.com>
2026-04-09 09:49:33 +0300

    adalineGD  + comparison of learning speed depending on the parameter eta

--- fastTest.py
+++ fastTest.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 from perceptron import Perceptron
+from AdaptiveLinearNeuron import AdalineGD
 from IrisDataSet import *
 from plot_decision_regions import plot_decision_regions
 
@@ -18,8 +19,29 @@ ppn.fit(X, y)
 
 # test plot decision regions func
 
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel("длина чашел.")
-plt.ylabel("длина лепестка")
-plt.legend(loc='upper left')
+# plot_decision_regions(X, y, classifier=ppn)
+# plt.xlabel("длина чашел.")
+# plt.ylabel("длина лепестка")
+# plt.legend(loc='upper left')
+# plt.show()
+
+
+# test adaline with gradient descent 
+# comparison of learning speed
+
+ada0 = AdalineGD(n_iter=15, eta=0.1)
+ada0.fit(X, y)
+
+ada1 = AdalineGD(n_iter=15, eta=0.0001)
+ada1.fit(X, y)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
+ax[0].plot(range(1, len(ada0.losses_) + 1), np.log10(ada0.losses_) ,marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log mse')
+ax[0].set_title('adaline eta=0.1')
+ax[1].plot(range(1, len(ada1.losses_) + 1), ada1.losses_ ,marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('mse')
+ax[1].set_title('adaline eta=0.0001')
 plt.show()
