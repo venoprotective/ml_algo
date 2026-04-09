6d1edf4e64bb454c356fe778f69278ca0b5c9b1c
venoprotective <venoprotective@gmail.com>
2026-04-09 10:07:47 +0300

    testing after standardization

--- IrisDataSet.py
+++ IrisDataSet.py
@@ -16,7 +16,6 @@ y = np.where(y == 'Iris-setosa', 0, 1)
 
 X = df.iloc[0:100, [0, 2]].values
 
-
 # print(df.tail())  
 #        0    1    2    3               4
 # 145  6.7  3.0  5.2  2.3  Iris-virginica
@@ -24,3 +23,13 @@ X = df.iloc[0:100, [0, 2]].values
 # 147  6.5  3.0  5.2  2.0  Iris-virginica
 # 148  6.2  3.4  5.4  2.3  Iris-virginica
 # 149  5.9  3.0  5.1  1.8  Iris-virginica
+
+# улучшим градиентный спуск с помощью масштабирования признаков
+# standardization
+# (x_j)' = (x_j - m_j) / o_j, где x_j - вектор из значений j-того признака всех обучающих образцов n
+# o_j - стандартное отклонение 
+# m_j - среднее значение выборки 
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
--- fastTest.py
+++ fastTest.py
@@ -29,19 +29,38 @@ ppn.fit(X, y)
 # test adaline with gradient descent 
 # comparison of learning speed
 
-ada0 = AdalineGD(n_iter=15, eta=0.1)
-ada0.fit(X, y)
+# ada0 = AdalineGD(n_iter=15, eta=0.1)
+# ada0.fit(X, y)
 
-ada1 = AdalineGD(n_iter=15, eta=0.0001)
-ada1.fit(X, y)
+# ada1 = AdalineGD(n_iter=15, eta=0.0001)
+# ada1.fit(X, y)
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
-ax[0].plot(range(1, len(ada0.losses_) + 1), np.log10(ada0.losses_) ,marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log mse')
-ax[0].set_title('adaline eta=0.1')
-ax[1].plot(range(1, len(ada1.losses_) + 1), ada1.losses_ ,marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('mse')
-ax[1].set_title('adaline eta=0.0001')
+# fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
+# ax[0].plot(range(1, len(ada0.losses_) + 1), np.log10(ada0.losses_) ,marker='o')
+# ax[0].set_xlabel('Epochs')
+# ax[0].set_ylabel('log mse')
+# ax[0].set_title('adaline eta=0.1')
+# ax[1].plot(range(1, len(ada1.losses_) + 1), ada1.losses_ ,marker='o')
+# ax[1].set_xlabel('Epochs')
+# ax[1].set_ylabel('mse')
+# ax[1].set_title('adaline eta=0.0001')
+# plt.show()
+
+
+# testing after standardization 
+ada = AdalineGD(n_iter=20, eta=0.5)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline with GD')
+plt.xlabel('длина чашел.')
+plt.ylabel('длина лепестка')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# measurement MSE 
+plt.plot(range(1, len(ada.losses_) + 1), ada.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('MSE')
+plt.tight_layout()
 plt.show()
