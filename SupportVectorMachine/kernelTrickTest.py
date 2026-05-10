0a6bc66b7b7f3a8e6dc9a196f0af7dcabb647f7d
venoprotective <venoprotective@gmail.com>
2026-05-10 13:18:13 +0300

    create sklearnDecisionTree.py

--- SupportVectorMachine/kernelTrickTest.py
+++ SupportVectorMachine/kernelTrickTest.py
@@ -7,7 +7,7 @@ from irisDataSet import *
 from plot_decision_regions import plot_decision_regions
 
 
-svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+svm = SVC(kernel='rbf', random_state=1, gamma=0.1, C=1000.0)
 
 # svm.fit(X_xor, y_xor)
 # plot_decision_regions(X=X_xor, y=y_xor, classifier=svm)
@@ -15,10 +15,10 @@ svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
 # plt.tight_layout()
 # plt.show()
 
-# svm.fit(X_train_std, y_train)
-# plot_decision_regions(X=X_combined_std, y=y_combined, classifier=svm, test_idx=range(105, 150))
-# plt.legend(loc='upper left')
-# plt.xlabel('Length petal [standardized]')
-# plt.ylabel('Width petal [standardized]')
-# plt.tight_layout()
-# plt.show()
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=svm, test_idx=range(105, 150))
+plt.legend(loc='upper left')
+plt.xlabel('Length petal [standardized]')
+plt.ylabel('Width petal [standardized]')
+plt.tight_layout()
+plt.show()
