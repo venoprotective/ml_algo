b133a4345c952cb71779cb901bfe1cb069c444b1
venoprotective <venoprotective@gmail.com>
2026-05-12 11:48:20 +0300

    add diagram

--- DecisionTree/sklearnDecisionTree.py
+++ DecisionTree/sklearnDecisionTree.py
@@ -1,4 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier 
+from sklearn import tree
 from pathlib import Path
 import sys 
 sys.path.append(str(Path(__file__).parent.parent))
@@ -9,13 +10,18 @@ import matplotlib.pyplot as plt
  
 tree_model = DecisionTreeClassifier(criterion='gini', max_depth=4, random_state=1)
 tree_model.fit(X_train, y_train)
+if (str(input("input 1 if u want watch plot else diagram: ")) == '1'):
+    X_combined = np.vstack((X_train, X_test))
+    y_combined = np.hstack((y_train, y_test))
 
-X_combined = np.vstack((X_train, X_test))
-y_combined = np.hstack((y_train, y_test))
-
-plot_decision_regions(X_combined, y_combined, tree_model, test_idx=range(105,150))
-plt.xlabel('length petal')
-plt.ylabel('width petal')
-plt.legend(loc='upper left')
-plt.tight_layout()
-plt.show()
+    plot_decision_regions(X_combined, y_combined, tree_model, test_idx=range(105,150))
+    plt.xlabel('length petal')
+    plt.ylabel('width petal')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
+else:
+    feature_names = ['sepal length', 'sepal width',
+                     'petal length', 'petal width']
+    tree.plot_tree(tree_model, feature_names=feature_names, filled=True)
+    plt.show()
